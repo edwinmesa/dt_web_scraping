d33a403826b5ea0dc378d012c16bc11fb73cc80a
Edwin Mesa <edwinsar.mes@gmail.com>
2022-08-22 09:54:37 -0500

    changes code

--- prod/exito/web_scraping_exito_v3.py
+++ prod/exito/web_scraping_exito_v3.py
@@ -65,7 +65,7 @@ def process_data():
     time.sleep(0.02)
 
 # Categories of brands that should be considered for search results
-categories = ['whisky-ron-brandy-conac', 'vinos','cervezas', 'tequilas-ginebras-y-vodkas'] 
+# categories = ['whisky-ron-brandy-conac', 'vinos','cervezas', 'tequilas-ginebras-y-vodkas'] 
 
 shops = {'Bogotá, D.c.': 'EXITO Calle 80', 'Medellín': 'Exito Envigado','Barranquilla':'Exito Barranquilla'}
 
@@ -74,117 +74,112 @@ shops = {'Bogotá, D.c.': 'EXITO Calle 80', 'Medellín': 'Exito Envigado','Barra
 # ------------------------------------------------------------------
 
 for city, suc in shops.items():
-    for category in categories:
-        # Bar progress -> comment
-        for _ in track(range(100), description=f'[green]Iniciando Scraping Almacenes EXITO ciudad {city} categoria: {category}'):
-            process_data()
-        # Initialized by selenium driver with options and optmizer
-        options=Options()
-        options.set_preference("network.http.pipelining", True)
-        options.set_preference("network.http.proxy.pipelining", True)
-        options.set_preference("network.http.pipelining.maxrequests", 8)
-        options.set_preference("content.switch.threshold", 250000)
-        options.set_preference("browser.cache.memory.capacity", 65536)
-        options.set_preference("general.startup.browser", False)
-        options.set_preference("reader.parse-on-load.enabled", False) # Disable reader, we won't need that.
-        options.set_preference("browser.pocket.enabled", False)
-        options.set_preference("loop.enabled", False)
-        options.set_preference("browser.chrome.toolbar_style", 1) # Text on Toolbar instead of icons
-        options.set_preference("browser.display.show_image_placeholders", False) # Don't show thumbnails on not loaded images.
-        options.set_preference("browser.display.use_document_colors", False) # Don't show document colors.
-        options.set_preference("browser.display.use_document_fonts", 0) # Don't load document fonts.
-        options.set_preference("browser.display.use_system_colors", True) # Use system colors.
-        options.set_preference("browser.formfill.enable", False) # Autofill on forms disabled.
-        options.set_preference("browser.helperApps.deleteTempFileOnExit", True) # Delete temprorary files.
-        options.set_preference("permissions.default.image", 2) 
-        options.set_preference("browser.tabs.forceHide", True) # Disable tabs, We won't need that.
-        options.set_preference("browser.urlbar.autoFill", False) # Disable autofill on URL bar.
-        options.set_preference("browser.urlbar.autocomplete.enabled", False) # Disable autocomplete on URL bar.
+    # Bar progress -> comment
+    for _ in track(range(100), description=f'[green]Iniciando Scraping Almacenes EXITO ciudad {city}'):
+        process_data()
+    # Initialized by selenium driver with options and optmizer
+    options=Options()
+    options.set_preference("network.http.pipelining", True)
+    options.set_preference("network.http.proxy.pipelining", True)
+    options.set_preference("network.http.pipelining.maxrequests", 8)
+    options.set_preference("content.switch.threshold", 250000)
+    options.set_preference("browser.cache.memory.capacity", 65536)
+    options.set_preference("general.startup.browser", False)
+    options.set_preference("reader.parse-on-load.enabled", False) # Disable reader, we won't need that.
+    options.set_preference("browser.pocket.enabled", False)
+    options.set_preference("loop.enabled", False)
+    options.set_preference("browser.chrome.toolbar_style", 1) # Text on Toolbar instead of icons
+    options.set_preference("browser.display.show_image_placeholders", False) # Don't show thumbnails on not loaded images.
+    options.set_preference("browser.display.use_document_colors", False) # Don't show document colors.
+    options.set_preference("browser.display.use_document_fonts", 0) # Don't load document fonts.
+    options.set_preference("browser.display.use_system_colors", True) # Use system colors.
+    options.set_preference("browser.formfill.enable", False) # Autofill on forms disabled.
+    options.set_preference("browser.helperApps.deleteTempFileOnExit", True) # Delete temprorary files.
+    options.set_preference("permissions.default.image", 2) 
+    options.set_preference("browser.tabs.forceHide", True) # Disable tabs, We won't need that.
+    options.set_preference("browser.urlbar.autoFill", False) # Disable autofill on URL bar.
+    options.set_preference("browser.urlbar.autocomplete.enabled", False) # Disable autocomplete on URL bar.
 
-        driver = webdriver.Firefox(options=options)
-        driver.maximize_window()
+    driver = webdriver.Firefox(options=options)
+    driver.maximize_window()
 
-        # Open the Page
-        if category == "whisky-ron-brandy-conac":
-            driver.get(f"https://www.exito.com/licores/{category}")
-        else:
-            driver.get(f"https://www.exito.com/mercado/vinos-y-licores/{category}")    
-        time.sleep(12)
-    
-        findElementBy(
-            By.XPATH, "//div[@class='exito-geolocation-3-x-contentOrderOption flex']//div[1]", 2)
-        # Click for city selection
-        findElementBy(
-            By.CSS_SELECTOR, ".exito-geolocation-3-x-orderOptionsButton.orderoption-compra-recoge", 2)
-        # List of cities
-        findElementByAndSendKey(
-            By.ID, "react-select-2-input", city, 2)
-        findElementByAndSendKey(
-            By.ID, "react-select-4-input", suc, 2)
-        findElementBy(By.XPATH, "//button[normalize-space()='Confirmar']", 2)
+    # Open the Page
+    driver.get(f"https://www.exito.com/mercado/vinos-y-licores")    
 
-        # For security reasons, we used twice the function because the page is refresh
-        # scrollDownPage(driver, 5)
-        scrollDownPage(driver, 10)
+    findElementBy(
+        By.XPATH, "//div[@class='exito-geolocation-3-x-contentOrderOption flex']//div[1]", 2)
+    # Click for city selection
+    findElementBy(
+        By.CSS_SELECTOR, ".exito-geolocation-3-x-orderOptionsButton.orderoption-compra-recoge", 2)
+    # List of cities
+    findElementByAndSendKey(
+        By.ID, "react-select-2-input", city, 2)
+    findElementByAndSendKey(
+        By.ID, "react-select-4-input", suc, 2)
+    findElementBy(By.XPATH, "//button[normalize-space()='Confirmar']", 2)
 
-        initial_XPATH = "//div[contains(@class,'vtex-button__label flex items-center justify-center h-100 ph5')]"
-        # define the max clicks for page for default 30
-        max_click_SHOW_MORE = 20
-        # count the number of clicks
-        count = 1
-        # This loop search the button load more and apply the click until the end of page
-        while count <= max_click_SHOW_MORE:
-            try:
-                WebDriverWait(driver, 30).until(
-                    EC.visibility_of_all_elements_located((By.XPATH, initial_XPATH)))
-                WebDriverWait(driver, 20).until(
-                    EC.element_to_be_clickable((By.XPATH, initial_XPATH))).click()
-                count += 1
-                time.sleep(10)
-                # Bar progress -> comment
-                for i in track(range(4), description=f"[red]Explorando Pagina Web iter {count - 1}.........."):
-                    time.sleep(1)
+    # For security reasons, we used twice the function because the page is refresh
+    # scrollDownPage(driver, 5)
+    scrollDownPage(driver, 10)
 
-            except TimeoutException:
-                break
+    initial_XPATH = "//div[contains(@class,'vtex-button__label flex items-center justify-center h-100 ph5')]"
+    # define the max clicks for page for default 30
+    max_click_SHOW_MORE = 35
+    # count the number of clicks
+    count = 1
+    # This loop search the button load more and apply the click until the end of page
+    while count <= max_click_SHOW_MORE:
+        try:
+            WebDriverWait(driver, 30).until(
+                EC.visibility_of_all_elements_located((By.XPATH, initial_XPATH)))
+            WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, initial_XPATH))).click()
+            count += 1
+            time.sleep(10)
+            # Bar progress -> comment
+            for i in track(range(4), description=f"[red]Explorando Pagina Web iter {count - 1}.........."):
+                time.sleep(1)
 
-        # Search the elements of the page
-        items = driver.find_elements(
-            By.CSS_SELECTOR,  ".vtex-product-summary-2-x-element.pointer.pt3.pb4.flex.flex-column.h-100")
-        # Create a frame empty for the data
-        data_exito = []
-        # iterate over each element
-        for i in items:
-            name = findElementTextBySelector(
-                ".vtex-store-components-3-x-productNameContainer.mv0.t-heading-4", "SIN DESCRIPCION")
-            brand = findElementTextBySelector(
-                ".vtex-product-summary-2-x-productBrandName", "SIN MARCA")
-            price_prime = findElementNumberBySelector(
-                ".exito-vtex-components-4-x-valuePLPAllied", "0")    
-            price_regular = findElementNumberBySelector(
-                ".exito-vtex-components-4-x-list-price.t-mini.ttn.strike", "0")
-            price_now = findElementNumberBySelector(
-                ".exito-vtex-components-4-x-PricePDP", "0")
-            discount = findElementNumberBySelector(
-                ".exito-vtex-components-4-x-badgeDiscount.flex.items-center", "0")
+        except TimeoutException:
+            break
 
-            data_exito.append({f"shop": "EXITO",
-                               "city": city,
-                               "location": suc,
-                               "category": category,
-                               "name": name,
-                               "brand": brand,
-                               "price_prime": price_prime,
-                               "price_regular": price_regular,
-                               "price_now": price_now,
-                               "discount": discount})
+    # Search the elements of the page
+    items = driver.find_elements(
+        By.CSS_SELECTOR,  ".vtex-product-summary-2-x-element.pointer.pt3.pb4.flex.flex-column.h-100")
+    # Create a frame empty for the data
+    data_exito = []
+    # iterate over each element
+    for i in items:
+        name = findElementTextBySelector(
+            ".vtex-store-components-3-x-productNameContainer.mv0.t-heading-4", "SIN DESCRIPCION")
+        brand = findElementTextBySelector(
+            ".vtex-product-summary-2-x-productBrandName", "SIN MARCA")
+        price_prime = findElementNumberBySelector(
+            ".exito-vtex-components-4-x-valuePLPAllied", "0")    
+        price_regular = findElementNumberBySelector(
+            ".exito-vtex-components-4-x-list-price.t-mini.ttn.strike", "0")
+        price_now = findElementNumberBySelector(
+            ".exito-vtex-components-4-x-PricePDP", "0")
+        discount = findElementNumberBySelector(
+            ".exito-vtex-components-4-x-badgeDiscount.flex.items-center", "0")
 
-        df = pd.DataFrame(data_exito)
-        df.to_csv(f'C:\workflow\dt_web_scraping\prod\data\exito_{city}_{suc}_{category}_data.txt',
-                  index=False, encoding='utf-8')
+        data_exito.append({f"shop": "EXITO",
+                            "city": city,
+                            "location": suc,
+                            "category": "Todas",
+                            "name": name,
+                            "brand": brand,
+                            "price_prime": price_prime,
+                            "price_regular": price_regular,
+                            "price_now": price_now,
+                            "discount": discount})
 
-        time.sleep(1)
-        driver.quit()
+    df = pd.DataFrame(data_exito)
+    df.to_csv(f'C:\workflow\dt_web_scraping\prod\data\exito_{city}_{suc}_data.txt',
+                index=False, encoding='utf-8')
+
+    time.sleep(1)
+    driver.quit()
 
 
 time.sleep(3)
