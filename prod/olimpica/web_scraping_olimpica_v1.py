c88fb4d058ea3ea5bdc535c48d1db9d7b1b9e501
Edwin Sair <edwinsar.mes@gmail.com>
2022-08-30 12:00:43 -0500

    code olimpica stable v1

--- prod/olimpica/web_scraping_olimpica_v1.py
+++ prod/olimpica/web_scraping_olimpica_v1.py
@@ -74,10 +74,10 @@ def scrollDownFullPage(driver):
     height = driver.execute_script("return document.body.scrollHeight")
     for i in range(height):
         # scroll by 10 on each iteration
-        driver.execute_script('window.scrollBy(0,40)')
+        driver.execute_script('window.scrollBy(0,30)')
         # reset height to the new height after scroll-triggered elements have been loaded.
         height = driver.execute_script("return document.body.scrollHeight")
-        time.sleep(0.15)
+        time.sleep(0.50)
 
 # Function Beatiful View
 
@@ -144,11 +144,9 @@ for city, suc in shops.items():
         By.XPATH, f"//div[contains(text(),'{suc}')]", 2)
 
     findElementBy(
-        By.XPATH, "//div[normalize-space()='Elegir']", 7)    
+        By.XPATH, "//div[normalize-space()='Elegir']", 2)    
 
     scrollDownFullPage(driver)
-
-    time.sleep(10)
 
     # Search the elements of the page
     items = driver.find_elements(
