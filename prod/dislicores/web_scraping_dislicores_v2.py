87384cd8f5145b116b00c564d7e528895962eeda
Edwin Mesa <edwinsar.mes@gmail.com>
2022-10-04 10:50:44 -0500

    change in times

--- prod/dislicores/web_scraping_dislicores_v2.py
+++ prod/dislicores/web_scraping_dislicores_v2.py
@@ -175,7 +175,7 @@ for city in shops:
 
         initial_XPATH = "//div[contains(@class,'vtex-button__label flex items-center justify-center h-100 ph5')]"
         # define the max clicks for page for default 30
-        max_click_SHOW_MORE = 50
+        max_click_SHOW_MORE = 45
         # count the number of clicks
         count = 1
         # This loop search the button load more and apply the click until the end of page
